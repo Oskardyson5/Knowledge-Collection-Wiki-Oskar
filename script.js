035a6d0a8387cb56a77cfedeb6b22c3a0211ef10
Oskar <kind.o.famiilie@gmail.com>
2026-07-29 08:34:00 +0200

    Ordnerstruktur 1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const folders = [
+    "Privat",
+    "Schule",
+    "Studium"
+];
+
+const folderTree = document.getElementById("folder-tree");
+
+foldeers.forEach(function(foldername){
+    folderTree.innerHTML +=
+    <nav class="choosebutton">
+        <div>
+            <h2>📁 ${folderName}</h2>
+        </div>
+    </nav>
+})
+
+
 const newButton = document.getElementById("newButton");
 const createMenu = document.getElementById("create-menu");
 
@@ -7,7 +25,7 @@ newButton.addEventListener("click", function() {
       <div class="create-window">
          <h3>Was möchtest du erstellen?</h3>
          <a href="ordner.html" class="create-button">📁 Ordner</a>
-         <a href="artikel.html">📄 Eintrag</a>
+         <a href="artikel.html" class="create-button">📄 Eintrag</a>
         </div>
     `;
 
